6ce045d2b38ba5b22e0bdd69ed73302bd9f9c3b3
Killian <killian2100@hotmail.com>
2020-01-04 20:18:25 +0000

    Problem 1

---
+++ p1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int total = 0;
+    for (int i = 1; i < 1000; i++){
+        if( !(i % 3) || !(i % 5) ){
+            printf("%i, ", i);
+            total = total + i;
+        }
+    }
+    printf("\n%i", total);
+    return 0;
+}
